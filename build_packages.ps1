2973f3da61f9a02cfa89a660a371412c2d281501
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2020-12-13 00:57:06 +0100

    NuGet packages, missing swnfd/freetype builds

---
+++ build_packages.ps1
@@ -0,0 +1,9 @@
+#!/usr/bin/env pwsh
+
+Set-Location (Split-Path ($MyInvocation.MyCommand.Path))
+
+New-Item -Force -ItemType "directory" packages_build
+
+Get-ChildItem -Recurse Packages/*.nupkg | Remove-Item
+Get-ChildItem -Directory Packages | ForEach-Object -Parallel { dotnet pack "$_/$($_.Name).csproj" }
+Get-ChildItem -Recurse Packages/*.nupkg | Copy-Item -Destination packages_build 
